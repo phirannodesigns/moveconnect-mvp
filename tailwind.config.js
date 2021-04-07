63ad077ff7f5427231ffcd78256ecd762cd7db5c
Luke Bennett <hello@lukebennett.com.au>
2021-04-07 13:20:02 +1000

    Initial commit from gatsby: (https://github.com/phirannodesigns/gatsby-starter.git)

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/* eslint-disable global-require, import/no-extraneous-dependencies */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    backdropFilter: {
+      blur: `blur(${defaultTheme.spacing[1]})`,
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Avenir Next', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('tailwindcss-filters'),
+  ],
+}
